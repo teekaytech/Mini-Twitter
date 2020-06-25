e7c8a69d86db1ea34ff296f3128c53f722a1e305
teekaytech <taofeek.olalere@feghas.com>
2020-06-25 16:24:47 +0100

    Modify routes to work with registrations controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_141533) do
+ActiveRecord::Schema.define(version: 2020_06_25_150040) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
